21787a1d06d88e843efe9e2808669b216dfc3728
Avinash Kumar <65714285+Avinashsingh011@users.noreply.github.com>
2026-07-29 17:12:34 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Welcome to my 1st java work");
+        System.out.println("Welcome to my 1st java work, Hello Avinash, Supriya, & Amaira. 
+                           welcome again to my Pool SCM Build check.");
     }
 }
